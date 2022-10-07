b8580881e3b78d5b8c01a894ff9b64c608469d99
Pius <mutundu.nguma@gmail.com>
2022-10-07 09:39:37 +0300

    more lines for my jave code

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var inPutBirthDate = prompt("Enter your brthday('YY-MM-DD')");
+console.log(inPutBirthDate);
+
+var genderInput = prompt ("Enter your gender ('Female or Male')")
+console.log(genderInput);
+var dayOfWeek =["Sunday","Monday" , "Tuesday", "Wednesday","Thursday","Friday","Sartuday"]
+var maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame",]
+var femaleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
