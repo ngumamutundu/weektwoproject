1b149af1a5b9763e5ce26cbc64a767bd8067cc2f
Pius <mutundu.nguma@gmail.com>
2022-10-07 19:47:42 +0300

    replace form with button

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ console.log(inPutBirthDate);
 
 var genderInput = prompt("Enter your gender ('Female or Male')");
 console.log(genderInput);
-var dayOfWeek = [
+const dayOfWeek = [
   "Sunday",
   "Monday",
   "Tuesday",
@@ -12,5 +12,5 @@ var dayOfWeek = [
   "Friday",
   "Sartuday",
 ];
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
