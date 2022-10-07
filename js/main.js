6a7e2cadaaed325c824448358eead309ab7f21eb
Pius <mutundu.nguma@gmail.com>
2022-10-07 13:28:42 +0300

    format my code with prettier

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 var inPutBirthDate = prompt("Enter your brthday('YY-MM-DD')");
 console.log(inPutBirthDate);
 
-var genderInput = prompt ("Enter your gender ('Female or Male')")
+var genderInput = prompt("Enter your gender ('Female or Male')");
 console.log(genderInput);
-var dayOfWeek =["Sunday","Monday" , "Tuesday", "Wednesday","Thursday","Friday","Sartuday"]
-var maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame",]
-var femaleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+var dayOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Sartuday",
+];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
