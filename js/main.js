d5ecedaed0a3a5ac6b9cc73520ec8b2a793c278b
Pius <mutundu.nguma@gmail.com>
2022-10-09 12:02:50 +0300

    link javascript to html to display the code

--- js/main.js
+++ js/main.js
@@ -1,21 +1,63 @@
+function akanNames() {
+    var daysOfTheWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ]; // shows days of the week
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-function(){
-    inPutBirthDate; genderInput;
+    var dateInput = prompt("Enter date of birth: YY-MM-DD:"); // requests user to put date of birth in indicated format.
 
-    var inPutBirthDate = prompt("Enter your brthday('YY-MM-DD')");
-console.log(inPutBirthDate);
+    var birthDate = new Date(dateInput); // fetches date from the user input
 
-var genderInput = prompt("Enter your gender ('Female or Male')");
-console.log(genderInput);
-const dayOfWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",           
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Sartuday",
-];
+    var dateBorn = birthDate.getDay(); // displays date of birth as  a number
+    var birthMonth = birthDate.getMonth();
+    var birthYear = birthDate.getFullYear();
+    var femaleGender = femaleNames[dateBorn]; //converts number into name of day
+    var maleGender = maleNames[dateBorn]; // converts number into name of day born
+
+
+    /*if (genderInput === "Male") {
+      alert("Your Akan name is" + maleGender);
+    } else {
+      alert("Your Akan name is" + femaleGender);
+    }*/
+
+    /*if ( genderInput ==="MALE" || genderInput==="FEMALE" || genderInput==="M" || genderInput==="F") {
+        alert("continue with in put");
+    } 
+    else {
+        alert("invalid");
+    }*/
+
+    if (
+        dateBorn >= 0 &&
+        dateBorn <= 6 &&
+        birthMonth >= 0 &&
+        birthMonth <= 11 &&
+        birthYear >= 1000
+    ) {
+        var genderInput = prompt("Enter your gender: Male or Female").toUpperCase;// validates genderInput
+        if (
+            genderInput !== "MALE" ||
+            genderInput !== "FEMALE" ||
+            genderInput !== "M" ||
+            genderInput !== "F"
+        ) {
+            if (genderInput !== "M" || genderInput !== "F" || genderInput !== "MALE" || genderInput !== "FEMALE") {
+                alert("Your Akan name is " + maleGender);
+            } else {
+                alert("Your Akan name is " + femaleGender);
+            }
+        } else {
+            alert("invalid gender use M/MALE or F/FEMALE");
+        }
+    } else {
+        alert("wrong date put date in this format YY-MM-DD");
+    }
 }
